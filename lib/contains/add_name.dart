bcb8c2c3dbeaa55a07a25b27c03bbbd22c7e6b3d
jainamjhaveri <engineerjainam@gmail.com>
2019-01-02 14:24:44 +0530

    remove transaction and directly use `setData` for adding baby name

--- lib/contains/add_name.dart
+++ lib/contains/add_name.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-class Anothername extends StatefulWidget{
+class Anothername extends StatefulWidget {
   State<StatefulWidget> createState() => Addname();
 }
 
@@ -12,68 +12,45 @@ class Addname extends State<Anothername> {
     return Scaffold(
       appBar: AppBar(title: Text('Baby Name Votes')),
       body: _BuildName(context),
-
     );
   }
-
 }
 
 Widget _BuildName(BuildContext context) {
   final TextEditingController namecontroller = TextEditingController();
 
   return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        children: <Widget>[
-          TextField(
-            controller: namecontroller,
-            decoration: InputDecoration.collapsed(
-                filled: true,
-                hintText: "Add a new name"
+    margin: const EdgeInsets.symmetric(horizontal: 8.0),
+    child: Column(
+      children: <Widget>[
+        TextField(
+          controller: namecontroller,
+          decoration: InputDecoration.collapsed(
+              filled: true, hintText: "Add a new name"),
         ),
-
+        SizedBox(height: 20.0),
+        ButtonBar(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Add'),
+              onPressed: () {
+                var document = Firestore.instance.collection('baby').document();
+                var map = new Map<String, dynamic>();
+                map['name'] = namecontroller.text;
+                map['votes'] = 0;
+                namecontroller.clear();
+                document.setData(map);
+              },
             ),
-          SizedBox(height: 20.0),
-
-          ButtonBar(
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Add'),
-                onPressed: (){
-
-                  Firestore.instance.runTransaction((transaction) async{
-                    final DocumentSnapshot ds= await transaction.get(Firestore.instance.collection('baby').document());
-
-                    var map = new Map<String,dynamic>();
-                    map['name']=namecontroller.text;
-                    map['votes']=0;
-                    namecontroller.clear();
-                    await transaction.set(ds.reference, map);
-                  }
-
-                  );
-
-
-
-                },
-
-              ),
-
-              RaisedButton(
-                child: Text('Clear'),
-                onPressed: (){
-                  namecontroller.clear();
-                },
-
-              ),
-
-
-
-
+            RaisedButton(
+              child: Text('Clear'),
+              onPressed: () {
+                namecontroller.clear();
+              },
+            ),
           ],
-          ),
-
-        ],
-      ),
+        ),
+      ],
+    ),
   );
 }
